a1a63e0147265cb5937948e5f5acdc8013cca46b
Yaradeottii <yaradeottii@gmail.com>
2026-03-13 12:18:21 -0300

    arrays

---
+++ array/array2.js
@@ -0,0 +1,9 @@
+const nomes = [];
+
+// desafio: inserir 5 novos indices com 5 nomes e imprimir na tela
+nomes [1] = "Yara"
+nomes [2] = "Emily"
+nomes [3] = "isa"
+nomes [4] = "katy"
+nomes [5] = "leti"
+console.log (nomes)
---
+++ array/desafio_array.js
@@ -0,0 +1,6 @@
+console.log(typeof console);
+console.log(typeof console.log);
+
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8 ,9, 10];
+console.log(typeof numeros) 
+console.log(numeros)
