af3df318712a0c2197c6053ad0347db3456ba752
Yaradeottii <yaradeottii@gmail.com>
2026-03-20 12:20:21 -0300

    Add files via upload

---
+++ forjs.js
@@ -0,0 +1,35 @@
+
+// Loop de 0 a 10
+
+console.log("=== Loop de 0 a 10 ===");
+for (let i = 0; i <= 10; i++) {
+    console.log("valor: " + i);
+}
+
+console.log("\n"); // linha em branco
+
+
+//  Verificar hora do dia
+
+let hora = 12;
+console.log("=== Verificar hora do dia ===");
+if (hora < 12) {
+    console.log("Bom dia!");
+} else if (hora > 12) {
+    console.log("Boa tarde!");
+} else {
+    console.log("É meio-dia!");
+}
+
+console.log("\n"); // linha em branco
+
+
+// Verificar idade
+
+let idade = 16;
+console.log("=== Verificar idade ===");
+if (idade >= 18) {
+    console.log("Você é maior de idade");
+} else {
+    console.log("Você é menor de idade");
+}
---
+++ termaros.js
@@ -0,0 +1,17 @@
+const hora = 22;
+let mensagem = hora < 12? "bom dia" : " boa tarde";
+console.log(mensagem);
+ 
+unsrios.js
+
+let a = 1;//1
+
+a = a + 1;//2
+a += 1;// 3
+a ++; // unario
+
+console.log(a); // 4
+
+a = a - 1; 
+a -= 1;
+a--; // unario
