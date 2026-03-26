9250d1a313487d50819142fd37c9e10c8d8874e3
Yaradeottii <yaradeottii@gmail.com>
2026-03-26 12:12:29 -0300

    atividade 3

---
+++ atividades/atvd3.js
@@ -0,0 +1,8 @@
+let idade = 21;
+let possuiConvite = true;
+
+if(idade >= 18 && possuiConvite == true){
+    console.log("você esta autorizado a entrar!");
+} else {
+     console.log("você NÃO esta autorizado a entrar!");
+}
